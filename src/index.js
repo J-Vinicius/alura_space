fc419169a9b12bcfb5b14006d8493730ed26da52
Jorge Vinicius <jorgeviniciuslucas@hotmail.com>
2023-08-04 14:15:11 -0300

    Aula-3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./assets/fonts/GanghiSans-Regular.otf";
+import "./assets/fonts/GanghiSans-Bold.otf";
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
